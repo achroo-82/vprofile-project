f6fad93095e9c2fd530c8942f86f595a3a6fcf72
achroobatta <achroo.batta@gmail.com>
2021-12-29 17:25:45 +0530

    cloudformation

--- bean-app.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_elastic_beanstalk_application" "bean-app" {
-  name = "vprofile-prod"
-}
--- keys.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "vprofilekey" {
-  key_name   = "vprofilekey"
-  public_key = file(var.PUBLIC_KEY_PATH)
-}
